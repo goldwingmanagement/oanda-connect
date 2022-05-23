e75cc4c5beb4d2fb91326e510ce4e2de14a575b9
Gold Wing Management <david.do@goldwingmanagement.com>
2022-05-22 23:46:19 -0400

    added write concern

--- src/index.ts
+++ src/index.ts
@@ -539,7 +539,13 @@ setInterval(() => {
             }
         });
     });
-    db.collection('market').bulkWrite(marketUpdates, err => {
+    db.collection('market').bulkWrite(marketUpdates, {
+        writeConcern: {
+            w: 0,
+            j: false,
+            wtimeout: 500
+        }
+    }, err => {
         logger.error(err);
     });
     let candlestickUpdates: any = [];
@@ -581,10 +587,22 @@ setInterval(() => {
             }
         });
     });
-    db.collection('candlestick').bulkWrite(candlestickUpdates, err => {
+    db.collection('candlestick').bulkWrite(candlestickUpdates, {
+        writeConcern: {
+            w: 0,
+            j: false,
+            wtimeout: 500
+        }
+    }, err => {
         logger.error(err);
     });
-    db.collection('timeframe').bulkWrite(timeframeUpdates, err => {
+    db.collection('timeframe').bulkWrite(timeframeUpdates, {
+        writeConcern: {
+            w: 0,
+            j: false,
+            wtimeout: 500
+        }
+    }, err => {
         logger.error(err);
     });
 }, 100)
